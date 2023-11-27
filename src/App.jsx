972f56d178a1f216ff70950bab0d1e39a8136d2e
Matheus França da Silva <matheus.francadasilva01@gmail.com>
2023-11-27 11:24:35 -0300

    Introdução ao React

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="App">
+        <h1>Este é um Hello World em React :D</h1>
+        <h2>Emmet está funcionando!</h2>
+        <button className='main-btn'>Pronto para seguir as aulas!</button>
+      </div>
+    </>
+  )
+}
+
+export default App
+
